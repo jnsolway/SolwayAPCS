4d84377262f913612ba4c603c5ed40ff75f0de40
Jeremiah Solway <solwaywashere@gmail.com>
2018-02-12 18:51:49 -0800

    Boolean

--- Functions/ValidationUtils.java
+++ Functions/ValidationUtils.java
@@ -2,24 +2,35 @@ import java.util.Scanner;
 
 class ValidationUtils {
     public static void main(String args[]) {
-        System.out.print("Enter a double: ");
         Scanner scan = new Scanner(System.in);
-        if(isValidDouble(scan)) {
-            System.out.println("Double Success!");
+        System.out.print("Enter a boolean: ");
+        if(isValidBoolean(scan)) {
+            System.out.println("Success!");
         }
         else {
-            System.out.println("Double Failure!");
+            System.out.println("Failure!");
         }
     }
 
     
-    public static Boolean isValidDouble(Scanner scan) {
-        try {
-            double i = scan.nextDouble();
-        }
-        catch(Exception e) {
-            return false; //An error was raised. The user input could not be read as a String.
-        }
-        return true; //No error was raised. The scanner read it successfully as a String.
+     public static Boolean isValidBoolean(Scanner scan) {
+         String s = scan.nextLine();
+        return s.equals("t")||
+            s.equals("f")||
+            s.equals("true")||
+            s.equals("false")||
+            s.equals("T")||
+            s.equals("F")||
+            s.equals("True")||
+            s.equals("False");
+
+    // public static Boolean isValidDouble(Scanner scan) {
+        // try {
+        //     double i = scan.nextDouble();
+        // }
+        // catch(Exception e) {
+        //     return false; //An error was raised. The user input could not be read as a String.
+        // }
+        // return true; //No error was raised. The scanner read it successfully as a String.
     }
 }
