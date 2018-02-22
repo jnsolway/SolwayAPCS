11e37f56e2fb5fdfcc3c3ad2bec4cfb10e306a2c
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-02-22 07:39:36 -0800

    For Loops

--- Assignments/Loops.java
+++ Assignments/Loops.java
@@ -7,26 +7,26 @@ class Loops {
 
         // for(int n = 1; n <= 10; n++) {
         //     System.out.println(n); 
-
-        // for(int i = 0; i <= 100; i += 2) { //prints even numbers between 0 and 100
+//prints even numbers between 0 and 100
+        // for(int i = 0; i <= 100; i += 2) { 
         //     System.out.println(i);
-
-        // for(int i = 0; i <= 100; i += 3) { //prints multiples of 3 and states wether they are odd or even
+//prints multiples of 3 and states wether they are odd or even
+        // for(int i = 0; i <= 100; i += 3) { 
         //     if(i % 2 == 0) {
         //         System.out.println(i + " is an even number");
         //     }
         //     else {
         //         System.out.println(i + " is an odd number");            
         //     }
-        
-        // for(int i = 0; i < 1000; i += i) { //prints 0 to 999
+//prints 0 to 999        
+        // for(int i = 0; i < 1000; i += i) { 
         //     System.out.println(i);//it an infinite loop   
-
-        // for(int i = 100; i < 0; i--) { //prints 100 to -1
+//prints 100 to -1
+        // for(int i = 100; i < 0; i--) { 
         //     System.out.println(i);
 
-        for(int i = 0; i > -100; i--) {
-            System.out.println(i);
+        // for(int i = 0; i > -100; i--) {
+        //     System.out.println(i);
     
         }       
     }
---
+++ Assignments/WhileLoops.java
@@ -0,0 +1,9 @@
+class WhileLoops {
+    public static void main(String[] args) {
+         int n = 0;
+        while(n < 100) {
+            System.out.println(n);
+            n += 5;
+        }
+    }
+}
