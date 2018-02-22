e2f2ab5d9c9d65bfebba6c225c4ce54f85b1b4d2
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-02-22 07:31:15 -0800

    For Loops

---
+++ Assignments/Loops.java
@@ -0,0 +1,33 @@
+class Loops {
+    public static void main(String[] args) {
+        // int n = 1;
+        // while(n <= 10) {
+        //     System.out.println(n);
+        //     n++;
+
+        // for(int n = 1; n <= 10; n++) {
+        //     System.out.println(n); 
+
+        // for(int i = 0; i <= 100; i += 2) { //prints even numbers between 0 and 100
+        //     System.out.println(i);
+
+        // for(int i = 0; i <= 100; i += 3) { //prints multiples of 3 and states wether they are odd or even
+        //     if(i % 2 == 0) {
+        //         System.out.println(i + " is an even number");
+        //     }
+        //     else {
+        //         System.out.println(i + " is an odd number");            
+        //     }
+        
+        // for(int i = 0; i < 1000; i += i) { //prints 0 to 999
+        //     System.out.println(i);//it an infinite loop   
+
+        // for(int i = 100; i < 0; i--) { //prints 100 to -1
+        //     System.out.println(i);
+
+        for(int i = 0; i > -100; i--) {
+            System.out.println(i);
+    
+        }       
+    }
+}
