836e17c4ff92a2ccf7c1bc5b94e9bae629ccb5c7
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-02-23 07:32:54 -0800

    While Loops

--- Assignments/WhileLoops.java
+++ Assignments/WhileLoops.java
@@ -1,9 +1,34 @@
 class WhileLoops {
     public static void main(String[] args) {
-         int n = 0;
-        while(n < 100) {
+        //  int n = 0;
+        // while(n < 100) {
+        //     System.out.println(n);
+        //     n += 5;
+        // }
+
+//Prints nothing
+        // int n = 19; 
+        // while(n % 2 == 0) {
+        //     System.out.println(n);
+        //     n--;        
+        // }
+
+//prints powers of 2 from 1 to 64
+    // int n = 1;
+    // while(n < 100) {
+    //     System.out.println(n);
+    //     n += n;        
+    // }
+
+//prints 0 to 20
+    Boolean shouldLoop = true;
+        int n = 0;
+        while(shouldLoop) {
+            if(n == 20) {
+                shouldLoop = false;
+            }
             System.out.println(n);
-            n += 5;
+            n++;
         }
     }
 }
