78c30dbbc95d0163027305a1c38caea06a3eabf8
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-02-26 07:22:42 -0800

    Challenge

--- Assignments/Challenge.java
+++ Assignments/Challenge.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Challenge {
     public static void main(String[] args) {
         Scanner scan;
+        System.out.println("=Enter a Valid Integer Within Three Tries=");
         for(int i = 0; i <= 2; i++) {
         scan = new Scanner(System.in);
             if(scan.hasNextInt()) { 
                 System.out.println("Success");
-            break;
+                break;
             } else if(i == 2) { 
                 System.out.println("Failure");
             }
