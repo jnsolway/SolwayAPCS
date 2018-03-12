e6ea101efd932afbb070ad0fca3ce6414e50c563
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-03-12 07:37:09 -0700

    ITERATE

--- Assignments/Iterating.java
+++ Assignments/Iterating.java
@@ -1,7 +1,7 @@
 class Iterating {
     public static void main(String[] args) {
         int[] nums = new int[201];
-        for(i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             nums[i] = i - nums.length/2;
             System.out.println(nums[i]);
         }
