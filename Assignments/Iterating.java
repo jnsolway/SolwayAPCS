6593ded52b84dbd47ddd4f7757e7c52f3d92bf08
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-03-13 07:02:23 -0700

    Iterating Data 1-3 finished, Working on Plinko

--- Assignments/Iterating.java
+++ Assignments/Iterating.java
@@ -1,9 +1,31 @@
 class Iterating {
     public static void main(String[] args) {
         int[] nums = new int[201];
+        int[] fibonnaci = new int[40];
         for(int i = 0; i < nums.length; i++) {
             nums[i] = i - nums.length/2;
-            System.out.println(nums[i]);
+            // System.out.println(nums[i]);
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] < 0){
+                System.out.println(nums[i]*-1);
+            } else {
+                System.out.println(nums[i]);
+            }
+        }
+        System.out.println("\n");
+       
+        // fibonnaci[1] = 1;
+        // System.out.println(fibonnaci[0]);
+        // System.out.println(fibonnaci[1]);
+        for(int i = 0; i < fibonnaci.length; i++) {
+            if(i < 2) {
+                fibonnaci[i] = i;   
+            }
+            if(i >= 2) {
+                fibonnaci[i] = fibonnaci[i-2] + fibonnaci[i-1];
+            }
+            System.out.println(fibonnaci[i]);
         }
     }
 }
