2042d6c0fd5150d1bfd9c71df7a598a38b959c25
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-03-13 16:35:10 -0700

    Fixed loanCalc Syntax and Quantum Plinko

--- Projects/LoanCalculator.java
+++ Projects/LoanCalculator.java
@@ -53,26 +53,26 @@ public class LoanCalculator{
         while(loanAmt <= 0) {
             System.out.print("Enter a Loan Amount: ");
             scan1 = new Scanner(System.in);
-                if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
-                    loanAmt = scan1.nextDouble();
-                }
+            if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
+                loanAmt = scan1.nextDouble();
+            }
         }
         // interestRate = scan.hasNextDouble();
         while(interestRate <= 0) {
             System.out.print("Interest Rate(" + ANSI_RED + "%" + ANSI_RESET + "): " );
             scan2 = new Scanner(System.in);
-                if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
-                    interestRate = scan2.nextDouble();
-                }
+            if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
+                interestRate = scan2.nextDouble();
+            }
         }
         
         // loanTerm = scan.hasNextInt();
         while(loanTerm <= 0) {
             System.out.print("Loan Term(Years): ");
             scan3 = new Scanner(System.in);
-                if(scan3.hasNextInt()) { //Check if input provided is a valid integer
-                    loanTerm = scan3.nextInt();
-                }
+            if(scan3.hasNextInt()) { //Check if input provided is a valid integer
+                loanTerm = scan3.nextInt();
+            }
         }
         double interest = loanAmt*interestRate/100*loanTerm;
         double loanCost = loanAmt+interest;
@@ -94,37 +94,37 @@ public class LoanCalculator{
         while(loanAmt <= 0) {
             System.out.print("Enter a Loan Amount: ");
             scan1 = new Scanner(System.in);
-                if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
-                    loanAmt = scan1.nextDouble();
-                }
+            if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
+                loanAmt = scan1.nextDouble();
+            }
         }
         // interestRate = scan.hasNextDouble();
         while(interestRate <= 0) {
             System.out.print("Interest Rate(" + ANSI_RED + "%" + ANSI_RESET + "): " );
             scan2 = new Scanner(System.in);
-                if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
-                    interestRate = scan2.nextDouble();
-                }
+            if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
+                interestRate = scan2.nextDouble();
+            }
         }
         
         // loanTerm = scan.hasNextInt();
         while(loanTerm <= 0) {
             System.out.print("Loan Term(Years): ");
             scan3 = new Scanner(System.in);
-                if(scan3.hasNextInt()) { //Check if input provided is a valid integer
-                    loanTerm = scan3.nextInt();
-                }
+            if(scan3.hasNextInt()) { //Check if input provided is a valid integer
+                loanTerm = scan3.nextInt();
+            }
         }
         
-    double loanCost = loanAmt*Math.pow(1+((interestRate/100)/12),12*loanTerm);
-    double interest = loanCost-loanAmt;
+        double loanCost = loanAmt*Math.pow(1+((interestRate/100)/12),12*loanTerm);
+        double interest = loanCost-loanAmt;
 
-    System.out.println(" ");
-    System.out.println("Original Loan Amount:" + ANSI_GREEN + " $" + ANSI_RESET + loanAmt);
-    System.out.println("Rate of Interest: " + interestRate + ANSI_RED + "%" + ANSI_RESET);
-    System.out.println("Loan Term in Years: " + loanTerm);
-    System.out.println("Interest to be Paid:" + ANSI_GREEN + " $" + ANSI_RESET + interest);
-    System.out.println("Total Cost of Loan:" + ANSI_GREEN + " $" + ANSI_RESET + loanCost);
+        System.out.println(" ");
+        System.out.println("Original Loan Amount:" + ANSI_GREEN + " $" + ANSI_RESET + loanAmt);
+        System.out.println("Rate of Interest: " + interestRate + ANSI_RED + "%" + ANSI_RESET);
+        System.out.println("Loan Term in Years: " + loanTerm);
+        System.out.println("Interest to be Paid:" + ANSI_GREEN + " $" + ANSI_RESET + interest);
+        System.out.println("Total Cost of Loan:" + ANSI_GREEN + " $" + ANSI_RESET + loanCost);
         
     }
         
@@ -138,27 +138,27 @@ public class LoanCalculator{
         while(loanAmt <= 0) {
             System.out.print("Enter a Loan Amount: ");
             scan1 = new Scanner(System.in);
-                if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
-                    loanAmt = scan1.nextDouble();
-                }
+            if(scan1.hasNextDouble()) { //Check if input provided is a valid integer
+                loanAmt = scan1.nextDouble();
+            }
         }
         // interestRate = scan.hasNextDouble();
         while(interestRate <= 0) {
             System.out.print("Interest Rate(" + ANSI_RED + "%" + ANSI_RESET + "): " );
             scan2 = new Scanner(System.in);
-                if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
-                    interestRate = scan2.nextDouble();
-                    break;
-                }
+            if(scan2.hasNextDouble()) { //Check if input provided is a valid integer
+                interestRate = scan2.nextDouble();
+                break;
+            }
         }
         
         // loanTerm = scan.hasNextInt();
         while(loanTerm <= 0) {
             System.out.print("Loan Term: ");
             scan3 = new Scanner(System.in);
-                if(scan3.hasNextInt()) { //Check if input provided is a valid integer
-                    loanTerm = scan3.nextInt();
-                }
+            if(scan3.hasNextInt()) { //Check if input provided is a valid integer
+                loanTerm = scan3.nextInt();
+            }
         }
         
         interestRate = interestRate/100;
@@ -166,23 +166,23 @@ public class LoanCalculator{
         double monthlyPayment = 1 + loanAmt * (ratePerMonth / (1 - Math.pow((1 + ratePerMonth), (loanTerm * - 12))));
         double outstandingBalance = loanAmt;
         
-  while(outstandingBalance >= 0) {
-        interest = outstandingBalance * (interestRate / 12);
-        interestPaid = interestPaid + interest;
-        outstandingBalance = outstandingBalance + interest - monthlyPayment;
+        while(outstandingBalance >= 0) {
+            interest = outstandingBalance * (interestRate / 12);
+            interestPaid = interestPaid + interest;
+            outstandingBalance = outstandingBalance + interest - monthlyPayment;
         }
     
         double finalPay = monthlyPayment + outstandingBalance;
         double loanCost = loanAmt + interestPaid;
     
-    System.out.println(" ");
-    System.out.println("Original Loan Amount:" + ANSI_GREEN + " $" + ANSI_RESET + loanAmt);
-    System.out.println("Rate of Interest:" + " " + (interestRate*100) + ANSI_RED + "%" + ANSI_RESET);
-    System.out.println("Loan Term in Years:" + " " + loanTerm);
-    System.out.println("Minimum Monthly Payment:" + ANSI_GREEN + " $" + ANSI_RESET + monthlyPayment);
-    System.out.println("Final Monthly Payment:" + ANSI_GREEN + " $" + ANSI_RESET + finalPay);
-    System.out.println("Interest to be Paid:" + ANSI_GREEN + " $" + ANSI_RESET + interestPaid);
-    System.out.println("Total Cost of Loan:" + ANSI_GREEN + " $" + ANSI_RESET + loanCost);
+        System.out.println(" ");
+        System.out.println("Original Loan Amount:" + ANSI_GREEN + " $" + ANSI_RESET + loanAmt);
+        System.out.println("Rate of Interest:" + " " + (interestRate*100) + ANSI_RED + "%" + ANSI_RESET);
+        System.out.println("Loan Term in Years:" + " " + loanTerm);
+        System.out.println("Minimum Monthly Payment:" + ANSI_GREEN + " $" + ANSI_RESET + monthlyPayment);
+        System.out.println("Final Monthly Payment:" + ANSI_GREEN + " $" + ANSI_RESET + finalPay);
+        System.out.println("Interest to be Paid:" + ANSI_GREEN + " $" + ANSI_RESET + interestPaid);
+        System.out.println("Total Cost of Loan:" + ANSI_GREEN + " $" + ANSI_RESET + loanCost);
 
     }
 
