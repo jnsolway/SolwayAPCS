bd273d00d1663d4adda2090693b5cbd4208b8b05
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-03-15 07:38:07 -0700

    Ability to add items to Todo List

--- Assignments/Lists.java
+++ Assignments/Lists.java
@@ -1,6 +1,16 @@
 import java.util.*;
-
+    
 class Lists {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    
     public static void main(String[] args) {
         // ArrayList<Integer> numList = new ArrayList<Integer>();
 
@@ -15,15 +25,17 @@ class Lists {
         // numList.remove(2); // [30,10,5,35,20]
 
         ArrayList<String> todoList = new ArrayList<String>(); 
-        
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        todoList.add(s);
-        // System.out.println(todoList.get(0));
-        for(int i = 0; i < todoList.size(); i++) {
-            System.out.println("[" + i + "] " + todoList.get(i));
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter an item in the To-do List: ");
+            String s = scan.nextLine();
+            todoList.add(s);
+            // System.out.println(todoList.get(0));
+            System.out.println(ANSI_RED + "To-do" + ANSI_RESET);
+            for(int i = 0; i < todoList.size(); i++) {
+                System.out.println("[" + i + "] " + todoList.get(i));
+            }
         }
-
 
     }   
 }
