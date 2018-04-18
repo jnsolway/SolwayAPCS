303952898de20bf14e8ebad2c680de11e06267d9
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-04-18 08:02:36 -0700

    Classes Assignemt

---
+++ Assignments/Arrays.java
@@ -0,0 +1,21 @@
+class Arrays {
+    public static void main(String[] args) {
+        int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+        System.out.println( numArray[3] ); //10
+
+        //System.out.println( numArray[11] ); //the array has position 0-10, 11 is out of bounds
+
+        System.out.println( numArray[1] ); //3
+
+        System.out.println( numArray[7] ); //6
+
+        System.out.println( numArray[8] ); //3
+
+
+
+
+
+
+    }
+}
--- Assignments/Classes/Example.java
+++ Assignments/Classes/Example.java
@@ -2,7 +2,10 @@ import utils.*;
 
 class Example {
     public static void main(String[] args) {
-        sentenceExample();
+        // sentenceExample();
+        // matrixTest1();
+        // matrixTest2();
+        matrixTest3();
     }
 
     public static void sentenceExample() {
@@ -10,4 +13,15 @@ class Example {
         System.out.println(sentence);
     }
 
+    public static void matrixTest1() {   
+        MatrixUtil.printMatrix(MatrixUtil.generateRandomMatrix(3,3));
+    }
+
+    public static void matrixTest2() {
+        MatrixUtil.printMatrix(MatrixUtil.generateRandomMatrix(4,4,1234));
+    }
+
+    public static void matrixTest3() {
+         MatrixUtil.printMatrix(MatrixUtil.reverseMatrix(MatrixUtil.generateRandomMatrix(4,4,1234)));
+    }
 }
