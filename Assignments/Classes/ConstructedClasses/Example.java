279d4f7e89b60f0e0a21d25ecec3a2d0d2fc143d
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-04-20 07:36:16 -0700

    Constucted Classes In Progress

---
+++ Assignments/Classes/ConstructedClasses/Example.java
@@ -0,0 +1,8 @@
+import school.*;
+
+class Example {
+    public static void main(String[] args) {
+        Student s = new Student("John", "Doe", 1234);
+        System.out.println(s.toString());
+    }
+}
