6ba4b1fa030d0ffa154cf8ef144a99ce153f7951
Jeremiah Solway <Solwa.Jere2018@ssusd.org>
2018-05-23 07:38:27 -0700

    Memory Game Begun

--- Projects/Memory/Game/MemoryGame.java
+++ Projects/Memory/Game/MemoryGame.java
@@ -4,13 +4,15 @@ public class MemoryGame {
     public int boardSize;
     public int pairAmounts;
     public int maxTries;
+    MemoryPiece[][] board;
 
     public MemoryGame(int boardSize, int pairAmounts, int maxTries) {
         this.boardSize = boardSize;
         this.pairAmounts = pairAmounts;
         this.maxTries = maxTries;
+
+        int totalShapes = boardSize * boardSize;
+        int numEachShape = totalShapes / pairAmounts;
     }
-    public GameBoard(){
-        int[][] board = new int[boardSize][boardSize];
-    }
+    
 }
---
+++ Projects/Memory/Game/MemoryPiece.java
@@ -0,0 +1,27 @@
+package game;
+
+public class MemoryPiece {
+    private boolean revealed;
+
+    public MemoryPiece() {
+        this.revealed = false;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void reveal() {
+        this.revealed = true;
+    }
+
+    public void unreveal() {
+        this.revealed = false;
+    }
+
+    public void render() {
+        System.out.print("[-]")
+    }
+
+    public abstract String getShapeType();
+}
